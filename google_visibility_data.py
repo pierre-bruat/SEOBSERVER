b65584f9c2f1139d9bdb631613b1a8aef3527ad2
Pierre Bruat <35839500+pierre-bruat@users.noreply.github.com>
2022-05-16 10:43:13 +0200

    cc

--- google_visibility_data.py
+++ google_visibility_data.py
@@ -69,11 +69,11 @@ dates = [dates_recent,WoW, MoM, QoQ]
 
 st.title('SEO visibility metrics 🇫🇷')
 st.write(f"last update {dates_recent}")
-url = f"AGG_DATA/final_data/raw_data_manomano_fr_fr_{dates_recent}.csv"
+url = f"SEOBSERVER/AGG_DATA/final_data/raw_data_manomano_fr_fr_{dates_recent}.csv"
 df_fr = pd.read_csv(url,index_col=0)
 st.dataframe(df_fr)
 header =st.header("Get ranking share 🇫🇷")
-df_spread = pd.read_csv(f"RAW_DATA/raw_data_manomano_fr_fr_{dates_recent}.csv")
+df_spread = pd.read_csv(f"SEOBSERVER/RAW_DATA/raw_data_manomano_fr_fr_{dates_recent}.csv")
 chart_2 = alt.Chart(df_spread).mark_bar().encode(x="date", y="count",color="Ranking range",tooltip=('Share','count')).properties(title="Share of ranking range")
 st.altair_chart(chart_2, use_container_width=True)
 
